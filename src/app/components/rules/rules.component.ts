6b457cc46eeadb78b5c9630c19c59469e3aebee6
yosi90 <yosi121990@hotmail.es>
2025-02-10 00:57:10 +0000

    RC2

--- src/app/components/rules/rules.component.ts
+++ src/app/components/rules/rules.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import {MatStepperModule} from '@angular/material/stepper';
 
 @Component({
     selector: 'app-rules',
-    imports: [CommonModule, MatDialogModule],
+    imports: [CommonModule, MatDialogModule, MatStepperModule],
     templateUrl: './rules.component.html',
     styleUrl: './rules.component.sass'
 })
